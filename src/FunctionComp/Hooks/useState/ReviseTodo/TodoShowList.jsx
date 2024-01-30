cf3c96bc1e379f76bfe2053680c1f0bcb9c7effa
Choudhary8055 <rahulchoudhary200599@gmail.com>
2024-01-30 23:02:42 +0530

    add delete function on todo list

--- src/FunctionComp/Hooks/useState/ReviseTodo/TodoShowList.jsx
+++ src/FunctionComp/Hooks/useState/ReviseTodo/TodoShowList.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
+import TodoUpdate from './TodoUpdate';
 
 const TodoShowList = ({todoData, setTodoData}) => {
     console.log("My data:", todoData); 
+
+    const deleteListItem = (itemId) => {
+         //remove itemId on list and return rest of array
+         const newUpdatedList = todoData.filter((item)=> item.id !== itemId);
+         setTodoData(newUpdatedList);
+    };
+
   return (
     <>
-      <ul>
+      <ol>
         {
-          todoData && todoData.map((item)=> (
+          Array.isArray(todoData) && todoData.map((item)=> (
             <>
-            <li key={item.id}>{item.title}</li>
+            <li key={item.id}>
+              <TodoUpdate  todoData={item} deleteListItem={deleteListItem} setTodoData={setTodoData}/>
+              {/* {item.title}
+              <button onClick={() => deleteListItem(item.id)}>Delete</button> */}
+            </li>
             </>
           ))
         }
-      </ul>
+      </ol>
     </>
   )
 }
--- src/FunctionComp/Hooks/useState/ReviseTodo/TodoUpdate.jsx
+++ src/FunctionComp/Hooks/useState/ReviseTodo/TodoUpdate.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const TodoUpdate = () => {
+const TodoUpdate = ({todoData, deleteListItem, setTodoData}) => {
+  const [editMode, setEditMode] = useState(false);
+
+  let todoContent;
+  if(editMode){
+//input and save button
+  todoContent = (
+    <>
+    <input type="text" value={todoData.title} 
+    onChange={(event)=> {
+      setTodoData({
+        ...todoData,
+        title: event.target.value
+      })
+    }}
+    />
+    <button onClick={()=> setEditMode(false)}>Save</button>
+    </>
+  )
+  }else{
+    //  title--> todoData and edit button
+    todoContent= (
+      <>
+      {todoData.title}
+      <button onClick={() => setEditMode(true)}>Edit</button>
+      </>
+    )
+  }
+
   return (
     <div>
-      
+      {/* <input type="checkbox"  /> */}
+      {todoContent}
+      <button onClick={()=> deleteListItem(todoData.id)}>Delete</button>
     </div>
   )
 }
 
 export default TodoUpdate
+/**
+ * edit --> save --> edit
+ */
